e760f0cfdaa38a4bf8fbc1984ee3fe4e46ad4ef8
Pedro O. A. Regis <pedroalencarregis@hotmail.com>
2024-10-22 18:55:52 -0300

    Add tests for missing fields

--- user/tests.py
+++ user/tests.py
@@ -19,7 +19,7 @@ class UserViewSetTestCase(BaseTestCase):
           - for feita a requisição de criação de um usuário
             `POST /api/user/`
         Então:
-          - a resposta deve ser HTTP_201_CREATED
+          - o status da resposta deve ser HTTP_201_CREATED
           - id do usuário deve está na resposta
           - a resposta não deve conter a senha
           - os demais campos devem ser iguais aos dados enviados
@@ -50,9 +50,9 @@ class UserViewSetTestCase(BaseTestCase):
           - for feita a requisição de criação de um usuário
             `POST /api/user/`
         Então:
-          - a resposta deve ser HTTP_400_BAD_REQUEST
+          - o status da resposta deve ser HTTP_400_BAD_REQUEST
         """
-        # TODO: why the UnicodeUsernameValidator is not working?
+        # TODO: why the UnicodeUsernameValidator is not working? If username=" Foo  Barr" it is not failing
         data = dict(
             username="",
             email="foo@bar.com",
@@ -61,19 +61,55 @@ class UserViewSetTestCase(BaseTestCase):
         )
 
         response = self.api.post(path="/api/user/", data=data)
-
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    def test_followers_1(self):
+    def test_create_3(self):
         """
         Dado:
-          - dois usuários cadastrados
-        Quando
+          - um payload sem email para cadastro de um usuário
+        Quando:
           - for feita a requisição de criação de um usuário
-            `POST /api/user/{user1.id}/followers/`
+            `POST /api/user/`
         Então:
-          - a resposta deve ser HTTP_201_CREATED
-          - a chave id deve está na resposta no campo results
+          - o status da resposta deve ser HTTP_400_BAD_REQUEST
+        """
+        data = dict(
+            username="foobar",
+            name="Foo",
+            password="1",
+        )
+
+        response = self.api.post(path="/api/user/", data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_4(self):
+        """
+        Dado:
+          - um payload sem password para cadastro de um usuário
+        Quando:
+          - for feita a requisição de criação de um usuário
+            `POST /api/user/`
+        Então:
+          - o status da resposta deve ser HTTP_400_BAD_REQUEST
+        """
+        data = dict(
+            username="foobar",
+            name="Foo",
+            email="foo@bar.com",
+        )
+
+        response = self.api.post(path="/api/user/", data=data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_follow_1(self):
+        """
+        Dado:
+          - dois usuários cadastrados em um segue o outro
+        Quando
+          - for feita a requisição de
+            `/api/user/{user1.id}/followers/`
+        Então:
+          - o status da resposta deve ser HTTP_200_OK
         """
         data1 = dict(
             username="foobar1",
@@ -91,7 +127,7 @@ class UserViewSetTestCase(BaseTestCase):
         user1 = User.objects.create(**data1)
         user2 = User.objects.create(**data2)
 
-        f = Follower.objects.create(following=user1, follower=user2)
+        Follower.objects.create(following=user1, follower=user2)
 
         response = self.api.get(path=f"/api/user/{user1.id}/followers/")
 
